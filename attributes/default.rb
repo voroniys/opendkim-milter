ff11686e95f313c6893fa796eb40afe85aba0705
Stanislav Voroniy <stas@voroniy.com>
2019-02-19 10:56:36 +0100

    No default configuration

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,26 +6,4 @@ else
   default['opendkim-milter']['os_targets'] = []
 end
 
-default['opendkim-milter']['services']['opendkim'] = {
-  service_name: 'opendkim',
-  config: {
-    PidFile:          '/var/run/opendkim/opendkim.pid',
-    Mode:             'v',
-    Syslog:           'yes',
-    SyslogSuccess:    'yes',
-    LogWhy:           'yes',
-    UserID:           'opendkim:opendkim',
-    Socket:           'local:/var/run/opendkim/opendkim.sock',
-    Umask:            '007',
-    OversignHeaders:  'From',
-  },
-  config_files: {
-    TrustedHosts: %w(127.0.0.1 ::1),
-    KeyTable: {
-      '#default._domainkey.example.com': 'example.com:default:/etc/opendkim/keys/default.private',
-    },
-    SigningTable: {
-      '#*@example.com': 'default._domainkey.example.com',
-    },
-  },
-}
+default['opendkim-milter']['services'] = {}
